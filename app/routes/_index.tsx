aa6eae9f79d5a04b22b4535a7a5fb87c56116801
rajivkhanna241989-hue <rajivkhanna241989@gmail.com>
2026-03-10 21:52:45 +1100

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "@remix-run/node";
+import { login } from "../shopify.server";
 
-export const loader = async () => {
+export const loader = async ({ request }: { request: Request }) => {
+  const url = new URL(request.url);
+  if (url.searchParams.get("shop")) {
+    throw await login(request);
+  }
   return redirect("/app");
 };
