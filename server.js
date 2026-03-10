bac1c6619ce1a526b82a5e76b62011c25d515c28
rajivkhanna241989-hue <rajivkhanna241989@gmail.com>
2026-03-10 15:42:31 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-import { createRequestHandler } from "@remix-run/serve";
-import { installGlobals } from "@remix-run/node";
-import * as fs from "fs";
+import { createRequestHandler } from "@remix-run/express";
+import express from "express";
 
-installGlobals();
+const app = express();
+app.use(express.static("build/client"));
 
-const BUILD_PATH = "./build/server/index.js";
-const PORT = process.env.PORT || 3000;
+app.all("*", createRequestHandler({
+  build: await import("./build/server/index.js"),
+}));
 
-const build = await import(BUILD_PATH);
-
-const handler = createRequestHandler(build, process.env.NODE_ENV);
-
-import { createServer } from "http";
-const server = createServer(handler);
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port " + (process.env.PORT || 3000));
 });
